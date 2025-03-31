cd742e8a26b646783bf8a182367862c1197acebb
JpedroA <pedrinhogt.alves@gmail.com>
2025-03-31 11:14:59 -0300

    função soma, igual funcional

---
+++ src/components/Input/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+background-color: #aaffaa;
+width: 100%;
+height: 75px;
+
+display: flex;
+align-items: center;
+justify-content: flex-end;
+
+font-size: 24px;
+font-family: 'Roboto', monospace;
+
+input{
+width: 100%;
+background-color: #aaffaa;
+border: 0;
+font-size: 24px;
+font-family: 'Roboto', monospace;
+padding: 0 20px;
+text-align: right;
+}
+`;
